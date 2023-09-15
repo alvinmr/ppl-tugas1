bfbe722152d58639cdd56e8dd3c6de0040e64535
alvin <maulanaalvin1@gmail.com>
2023-09-15 17:40:08 +0800

    init

---
+++ server/api/sqrt.ts
@@ -0,0 +1,34 @@
+export default defineEventHandler((event) => {
+    const number = Number(getQuery(event).number)
+
+    // validate the input
+    if (!Number.isInteger(number)) {
+        throw createError({
+            statusCode: 400,
+            statusMessage: 'The number must be greater than 0'
+        })
+    }
+
+    const sqrt = (number: number) => {
+        let guess = number / 2
+        let newGuess = 0
+        const start = performance.now()
+        while (guess !== newGuess) {
+            newGuess = guess
+            guess = (guess + number / guess) / 2
+        }
+        const end = performance.now()
+        
+        var executionTime = end - start
+        return {
+            number: number,
+            sqrt: guess,
+            executionTime: executionTime
+        }
+    }
+
+    return {
+        data: sqrt(number)
+    }
+})
+
