7c5044e76aac6d90f817a782381b94e36beca649
alvin <maulanaalvin1@gmail.com>
2023-10-28 22:03:56 +0800

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,5 +2,7 @@ export default defineNuxtRouteMiddleware((to) => {
     const auth = useAuthStore()
     if (!auth.isAuthenticated && to.path !== '/login') {
         return navigateTo('/login');
+    }else{
+        return navigateTo('/')
     }
 })
