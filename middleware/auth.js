288415b6af99465b25b24c7c991569bcbe65ae10
alvin <maulanaalvin1@gmail.com>
2023-10-31 11:30:17 +0800

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,5 @@ export default defineNuxtRouteMiddleware((to) => {
     const auth = useAuthStore()
     if (!auth.isAuthenticated && to.path !== '/login') {
         return navigateTo('/login');
-    }else{
-        return navigateTo('/')
     }
 })
--- stores/auth.js
+++ stores/auth.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
-        token: '',
+        token: null,
         user: null,
         isLoading: false,
     }),
@@ -13,29 +13,36 @@ export const useAuthStore = defineStore('auth', {
     actions: {
         async login(nim) {
             this.isLoading = true
-            await fetch('https://akar-kuadrat-api.fly.dev/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    nim,
-                })
-            })
-                .then((res) => res.json())
-                .then((user) => {
-                    this.isLoading = false
-                    if(!user.data) return { error: 'NIM tidak ditemukan'}
-                    this.token = user.data.token
-                    this.user = user.data
-                    localStorage.setItem('token', user.data.token)
-                }).catch((err) => {
-                    this.isLoading = false
-                    console.log(err)
-                })
+            this.user = {
+                name: nim,
+                username: nim,
+            }
+            this.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjIwMDU1NTEwMTQiLCJuYW1lIjoiQWx2aW4gTWF1bGFuYSBSaHVzdWxpIiwiZW1haWwiOiJtYXVsYW5hYWx2aW4xQGdtYWlsLmNvbSJ9.E5ahJ24X-yql8U4_qkwtTrJFNI_AncMf4CyhdC_Xx7w'
+            // await fetch('https://akar-kuadrat-api.fly.dev/api/login', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify({
+            //         nim,
+            //     })
+            // })
+            //     .then((res) => res.json())
+            //     .then((user) => {
+            //         this.isLoading = false
+            //         if(!user.data) return { error: 'NIM tidak ditemukan'}
+            //         this.token = user.data.token
+            //         this.user = user.data
+            //         localStorage.setItem('token', user.data.token)
+            //     }).catch((err) => {
+            //         this.isLoading = false
+            //         console.log(err)
+            //     })
         },
+
         logout() {
             this.token = ''
+            localStorage.removeItem('token')
         },
     },
 })
