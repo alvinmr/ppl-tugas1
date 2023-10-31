dbd7522b4825d2c6e201e431b2cf930c937e8c56
alvin <maulanaalvin1@gmail.com>
2023-10-31 11:48:43 +0800

    fix

--- stores/auth.js
+++ stores/auth.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
-        token: null,
+        token: '',
         user: null,
         isLoading: false,
     }),
